8914431f09b5f6a19eb2a95def307fb8c362471f
UeranJP <ueran.piazza@gmail.com>
2021-05-05 09:58:09 -0300

    Project Created

---
+++ backend/src/main/java/com/projetospring/psvendas/PsvendasApplication.java
@@ -0,0 +1,13 @@
+package com.projetospring.psvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsvendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/projetospring/psvendas/PsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetospring.psvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
