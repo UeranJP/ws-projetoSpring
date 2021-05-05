f75e3aec50907c766eaf18e959c6aff55ef1a433
UeranJP <ueran.piazza@gmail.com>
2021-05-05 11:09:22 -0300

    Basic Static Components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/UeranJP" target="_blank" rel="noreferrer">Ueran José Piazza</a></p>
+                <p className="text-light"><small><strong>Projeto Spring</strong></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
